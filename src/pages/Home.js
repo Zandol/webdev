afcf15f66ea0ca8b6acd3eb32f1fd2837b650e86
paturot <paturotsteeve@gmail.com>
2022-10-31 16:11:32 +0100

    section hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,16 @@ const Home = () => {
     return (
         <>
 
-            <h1>culinair</h1>
+            <div className="hero min-h-screen" style={{ backgroundImage: `url("asset/pik1.jpg")` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">CULIN'AIR</h1>
+                        <p className="mb-5">Le tourisme gastronomique à la française</p>
+                        <button className="btn btn-primary">bon appétit</button>
+                    </div>
+                </div>
+            </div>
 
         </>
     );
