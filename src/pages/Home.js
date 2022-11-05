1b6a9d484c95755ea8c57e523264d540277ecf78
paturot <paturotsteeve@gmail.com>
2022-11-05 16:01:26 +0100

    bootstrap modif

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Typography } from '@mui/material';
-
+import Carousel from 'react-bootstrap/Carousel';
 
 
 const Home = () => {
@@ -8,7 +7,66 @@ const Home = () => {
 
     return (
         <>
+            {/* <Carousel fade style={{ display: 'block', width: 1200, Height: 200, }}>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="plat.jpg"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="plat.jpg"
+                        alt="Second slide"
+                    />
 
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="plat.jpg"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel> */}
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="/plat.jpg" class="d-block w-100" alt="coucher du soleil" />
+                    </div>
+                    <div class="carousel-item">
+                        <img src="/plat.jpg" class="d-block w-100" alt="palmier" />
+                    </div>
+                    <div class="carousel-item">
+                        <img src="/plat.jpg" class="d-block w-100" alt="tournesol" />
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </>
     );
 }
